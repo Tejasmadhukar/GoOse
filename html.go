f2519ff56e78b828d52f597e010267c88e216822
Tejasmadhukar <39000441+Tejasmadhukar@users.noreply.github.com>
2023-11-14 21:45:42 +0530

    Updated Headers

--- html.go
+++ html.go
@@ -26,7 +26,20 @@ func (hr htmlrequester) fetchHTML(url string) (string, error) {
 	client.SetTimeout(hr.config.timeout)
 	resp, err := client.R().
 		SetHeader("Content-Type", "text/html").
-		SetHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.91 Safari/534.30").
+		SetHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36").
+		SetHeader("Accept", "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01").
+		SetHeader("Accept-Encoding", "gzip, deflate, br").
+		SetHeader("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8").
+		SetHeader("Cookie", "_gid=GA1.2.1990237071.1698737112; _gat_gtag_UA_46560124_1=1; _ga_VS4T955HM4=GS1.1.1698737111.1.1.1698738565.0.0.0; _ga=GA1.1.2108271516.1698737112").
+		SetHeader("Referer", "https://doaj.org/search/articles?ref=homepage-box&source=%7B%22query%22%3A%7B%22query_string%22%3A%7B%22query%22%3A%22climate%20change%22%2C%22default_operator%22%3A%22AND%22%7D%7D%2C%22size%22%3A%22200%22%2C%22track_total_hits%22%3Atrue%7D").
+		SetHeader("Sec-Ch-Ua", "\"Chromium\";v=\"118\", \"Google Chrome\";v=\"118\", \"Not=A?Brand\";v=\"99\"").
+		SetHeader("Sec-Ch-Ua-Mobile", "?0").
+		SetHeader("Sec-Ch-Ua-Platform", "\"macOS\"").
+		SetHeader("Sec-Fetch-Dest", "empty").
+		SetHeader("Sec-Fetch-Mode", "cors").
+		SetHeader("Sec-Fetch-Site", "same-origin").
+		SetHeader("X-Requested-With", "XMLHttpRequest").
+
 		Get(url)
 
 	if err != nil {
